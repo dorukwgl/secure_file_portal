975bdaf978092ffc48e98d0acc76a329fbcbc934
dorukwagle <dorukwagle@gmail.com>
2025-11-06 20:37:44 +0545

    implement file sharing

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import auth from "../api/auth/authController";
 import users from "../api/users/usersController";
 import {authorize} from "../middlewares/auth";
+import files from "../api/files/filesController";
 
 
 const api = (p: string) => `/api/${p}`;
@@ -12,6 +13,7 @@ const initializeRoutes = (app: Express): void => {
 
     app.use(api("users"), users);
     app.use(api("auth"), auth);
+    app.use(api("files"), files);
 }
 
 export default initializeRoutes;
